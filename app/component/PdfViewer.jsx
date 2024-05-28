ca3da42ac113068a679ee6b584b48f5120d863a1
Prashant Chauhan <2197prash@gmail.com>
2024-05-28 19:27:56 +0530

    fix: styling

--- app/component/PdfViewer.jsx
+++ app/component/PdfViewer.jsx
@@ -8,7 +8,7 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   import.meta.url
 ).toString();
 
-const PdfViewer = ({ pdfPath, pagination=false }) => {
+const PdfViewer = ({ pdfPath, pagination = false }) => {
   const {
     isLoading,
     numPages,
@@ -26,39 +26,42 @@ const PdfViewer = ({ pdfPath, pagination=false }) => {
   const renderPages = () => {
     const pages = [];
     for (let i = 1; i <= numPages; i++) {
-      pages.push(<><Page key={`page_${i}`} pageNumber={i} scale={scale} /> <p className="text-center">{`Page ${i}`}</p> </>);
-      
+      pages.push(
+        <>
+          <Page key={`page_${i}`} pageNumber={i} scale={scale} />{" "}
+          <p className="text-center">{`Page ${i}`}</p>{" "}
+        </>
+      );
     }
     return pages;
   };
 
- 
   return (
-    <div className="shadow-2xl max-w-screen-sm  mx-auto p-4">
-    <h1 className="text-lg font-semibold text-center mb-3 ">My PDF View</h1>
-          <PdfControls
-            numPages={numPages}
-            pageNumber={pageNumber}
-            goToPreviousPage={goToPreviousPage}
-            goToNextPage={goToNextPage}
-            zoomIn={zoomIn}
-            zoomOut={zoomOut}
-            onDownload={onDownload}
-            onPrint={onPrint}
-            pagination={pagination}
-          />
-          <div className=" max-h-screen overflow-y-auto">
-            <Document file={pdfPath} onLoadSuccess={onDocumentLoadSuccess}>
-              {pagination ? (<>
-                <Page pageNumber={pageNumber} scale={scale} />
-                <p className="text-center">{`Page ${pageNumber}`}</p>
-              </>
-              ) : (
-                renderPages()
-              )}
-            </Document>
-          </div>
-        
+    <div className="shadow-2xl max-w-screen-sm bg-slate-400 rounded-lg mx-auto p-3">
+      <h1 className="text-lg font-semibold text-center mb-3 ">My PDF View</h1>
+      <PdfControls
+        numPages={numPages}
+        pageNumber={pageNumber}
+        goToPreviousPage={goToPreviousPage}
+        goToNextPage={goToNextPage}
+        zoomIn={zoomIn}
+        zoomOut={zoomOut}
+        onDownload={onDownload}
+        onPrint={onPrint}
+        pagination={pagination}
+      />
+      <div className=" max-h-screen overflow-y-auto">
+        <Document file={pdfPath} onLoadSuccess={onDocumentLoadSuccess}>
+          {pagination ? (
+            <>
+              <Page pageNumber={pageNumber} scale={scale} />
+              <p className="text-center">{`Page ${pageNumber}`}</p>
+            </>
+          ) : (
+            renderPages()
+          )}
+        </Document>
+      </div>
     </div>
   );
 };
--- app/page.js
+++ app/page.js
@@ -2,9 +2,9 @@ import PdfViewer from "./component/PdfViewer";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-5">
+    <main className="flex min-h-screen bg-slate-100 flex-col items-center justify-between p-5">
       
-      <PdfViewer pdfPath="./document.pdf"   />
+      <PdfViewer pdfPath="./document.pdf"    />
 
     </main>
   );
